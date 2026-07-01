17bb17e54537b008f14716ac5bb6da705c38200c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-01 22:51:45 +0000

    safari-technology-preview 247

--- Casks/s/safari-technology-preview.rb
+++ Casks/s/safari-technology-preview.rb
@@ -12,8 +12,8 @@ cask "safari-technology-preview" do
   end
   # when adjusting the on_{os} scoping, also update the livecheck regex
   on_tahoe :or_newer do
-    version "246,140-18555-20260617-903317e6-560d-4370-9252-0be4d87523ea"
-    sha256 "bf34c0da8804ecfad524df1f0d271a7201cba138bb8130c9225aa0347a5b0c64"
+    version "247,140-34382-20260630-9f5e899d-3f85-450d-ab63-4f657b833cf9"
+    sha256 "c667953f7b84226cf7985db954b6743c90b71e4b6046bd7ebda494b841ebf50d"
 
     livecheck do
       url :homepage
